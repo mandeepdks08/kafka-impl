1ccf002b45b194df531ba75cab45d0a5f40b1d46
Mandeep Kumar <mandeepdks08@gmail.com>
2024-09-24 21:21:16 +0530

    push to partition

--- src/main/java/com/kafka/service/KafkaService.java
+++ src/main/java/com/kafka/service/KafkaService.java
@@ -1,9 +1,31 @@
 package com.kafka.service;
 
-public class KafkaService {
+import java.util.HashMap;
+import java.util.Map;
 
-	
+public class KafkaService {
+	private Map<String, Topic> topics;
+
+	public KafkaService() {
+		this.topics = new HashMap<>();
+	}
+
 	public void createTopic(String name, Integer partitions, Integer ttlInSeconds) {
-		
+		if (topics.containsKey(name)) {
+			throw new RuntimeException("Topic already exist");
+		}
+		Topic newTopic = new Topic(name, partitions, ttlInSeconds);
+		topics.put(name, newTopic);
+	}
+
+	public void push(String topic, String data) {
+		push(topic, data, null);
+	}
+
+	public void push(String topic, String data, String key) {
+		if (!topics.containsKey(topic)) {
+			throw new RuntimeException("Topic does not exist");
+		}
+		topics.get(topic).push(data, key);
 	}
 }
