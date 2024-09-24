17380254aa98b38e61f35299c123b229e62e3470
Mandeep Kumar <mandeepdks08@gmail.com>
2024-09-24 19:10:52 +0530

    partition class

--- src/main/java/com/kafka/service/Partition.java
+++ src/main/java/com/kafka/service/Partition.java
@@ -1,5 +1,53 @@
 package com.kafka.service;
 
-public class Partition {
+import org.apache.commons.lang3.ObjectUtils;
 
+import com.kafka.datamodel.Record;
+
+public class Partition {
+	private Record head;
+	private Record tail;
+	private Record next;
+
+	public void push(String data, Integer ttlInSeconds) {
+		Record newRecord = new Record(data, ttlInSeconds);
+		if (isPartitionEmpty()) {
+			head = newRecord;
+			tail = newRecord;
+			next = head;
+		} else {
+			tail.setNext(newRecord);
+			tail = newRecord;
+		}
+	}
+
+	public String poll() {
+		if (isPartitionEmpty()) {
+			return null;
+		}
+		if (next.isExpired()) {
+			next = head;
+		}
+		return next.getData();
+	}
+
+	public void acknowledge() {
+		if (next == null) {
+			next = head;
+		} else {
+			next = ObjectUtils.firstNonNull(next.getNext(), head);
+		}
+	}
+	
+	public void deleteExpiredRecords() {
+		while (head != null && head.isExpired()) {
+			Record previousRecord = head;
+			head = head.getNext();
+			previousRecord.setNext(null);
+		}
+	}
+
+	private boolean isPartitionEmpty() {
+		return head == null;
+	}
 }
