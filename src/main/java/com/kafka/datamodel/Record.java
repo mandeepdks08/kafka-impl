141ed116e86b6e75fe4e4501ba12774e2eb65bc2
Mandeep Kumar <mandeepdks08@gmail.com>
2024-09-29 13:23:07 +0530

    batch-wise data storage

--- src/main/java/com/kafka/datamodel/Record.java
+++ src/main/java/com/kafka/datamodel/Record.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 
 public class Record {
 	private String data;
-	private Record next;
 	private LocalDateTime expiration;
 
 	public Record(String data, Integer ttlInSeconds) {
@@ -14,7 +13,6 @@ public class Record {
 
 	public Record(String data, Record next, Integer ttlInSeconds) {
 		this.data = data;
-		this.next = next;
 		this.expiration = LocalDateTime.now().plusSeconds(ttlInSeconds);
 	}
 
@@ -22,14 +20,6 @@ public class Record {
 		return data;
 	}
 
-	public Record getNext() {
-		return next;
-	}
-
-	public void setNext(Record next) {
-		this.next = next;
-	}
-	
 	public boolean isExpired() {
 		return LocalDateTime.now().isAfter(expiration);
 	}
