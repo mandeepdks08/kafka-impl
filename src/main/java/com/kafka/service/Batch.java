67fbd9d0c72d1934d1259a2dbb60eca9566b1afa
Mandeep Kumar <mandeepdks08@gmail.com>
2024-09-29 16:30:23 +0530

    minor change

--- src/main/java/com/kafka/service/Batch.java
+++ src/main/java/com/kafka/service/Batch.java
@@ -10,21 +10,18 @@ public class Batch {
 	private final int startOffset;
 	private final int capacity;
 	private List<Record> records;
-	private int size;
 
 	public Batch(int startOffset, int capacity) {
 		this.startOffset = startOffset;
 		this.capacity = capacity;
 		this.records = new ArrayList<>(capacity);
-		this.size = 0;
 	}
 
 	public void append(Record record) {
 		if (isFull()) {
 			throw new RuntimeException("Batch full");
 		}
-		records.add(size + 1, record);
-		size++;
+		records.add(record);
 	}
 
 	public int getStartOffset() {
@@ -36,31 +33,31 @@ public class Batch {
 	}
 
 	public boolean isFull() {
-		return size == capacity;
+		return records.size() == capacity;
 	}
 
 	public int size() {
-		return size;
+		return records.size();
 	}
 
 	public boolean isEmpty() {
-		return size == 0;
+		return records.size() == 0;
 	}
 
 	public Record getRecord(int index) {
-		if (index < 0 || index >= size) {
+		if (index < 0 || index >= records.size()) {
 			throw new RuntimeException("Index out of bounds");
 		}
 		return records.get(index);
 	}
 
 	public boolean isExpired() {
-		return !isEmpty() && records.get(size - 1).isExpired();
+		return !isEmpty() && records.get(records.size() - 1).isExpired();
 	}
-	
+
 	public String convertToLog() {
 		StringBuilder log = new StringBuilder("");
-		for (Record record: records) {
+		for (Record record : records) {
 			log.append(GsonUtils.getGson().toJson(record));
 			log.append("\n");
 		}
